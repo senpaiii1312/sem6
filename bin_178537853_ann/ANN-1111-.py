3dc26d3364992a1dbb863ee6f92d6c29c43e6ad2
senpaiii1312 <spatil13122004@gmail.com>
2026-05-07 17:45:05 +0530

    s

--- bin_178537853_ann/ANN-1111-.py
+++ bin_178537853_ann/ANN-1111-.py
@@ -1,85 +1,29 @@
-import numpy as np
+from ultralytics import YOLO
+import cv2
+import os
+# Load YOLO model
+model = YOLO("yolov8n.pt")
+# Image path
+image_path = r"C:\Users\Sujal\Downloads\car.jpeg"
+# Check image
+if not os.path.exists(image_path):
+ print(" Image not found.")
+ exit()
+image = cv2.imread(image_path)
+# Run detection
+results = model(image)
+print("\nObject Detection Successful!\n")
+# Image information
+h, w, c = image.shape
+print(f"Image Resolution: {w} × {h} pixels\n") 
 
-digits = {
-    0: np.array([
-        [1, 1, 1],
-        [1, 0, 1],
-        [1, 0, 1],
-        [1, 0, 1],
-        [1, 1, 1]
-    ]),
-
-    1: np.array([
-        [0, 1, 0],
-        [1, 1, 0],
-        [0, 1, 0],
-        [0, 1, 0],
-        [1, 1, 1]
-    ]),
-
-    2: np.array([
-        [1, 1, 1],
-        [0, 0, 1],
-        [1, 1, 1],
-        [1, 0, 0],
-        [1, 1, 1]
-    ]),
-
-    3: np.array([
-        [1, 1, 1],
-        [0, 0, 1],
-        [1, 1, 1],
-        [0, 0, 1],
-        [1, 1, 1]
-    ]),
-
-    9: np.array([
-        [1, 1, 1],
-        [1, 0, 1],
-        [1, 1, 1],
-        [0, 0, 1],
-        [1, 1, 1]
-    ])
-}
-
-X = np.array([digits[d].flatten() for d in [0, 1, 2, 3, 9]])
-y = np.array([0, 1, 2, 3, 9])
-
-num_classes = 10
-targets = np.zeros((len(y), num_classes))
-
-for i in range(len(y)):
-    targets[i, y[i]] = 1
-
-weights = np.random.rand(15, num_classes) - 0.5
-bias = np.random.rand(num_classes) - 0.5
-
-learning_rate = 0.1
-epochs = 500
-
-def softmax(x):
-    exp_x = np.exp(x - np.max(x))
-    return exp_x / np.sum(exp_x)
-
-for _ in range(epochs):
-    for xi, target in zip(X, targets):
-        net = np.dot(xi, weights) + bias
-        output = softmax(net)
-
-        error = target - output
-
-        weights += learning_rate * np.outer(xi, error)
-        bias += learning_rate * error
-
-print("Training Completed\n")
-
-for digit in [0, 1, 2, 3, 9]:
-    test = digits[digit].flatten()
-    net = np.dot(test, weights) + bias
-    output = softmax(net)
-    prediction = np.argmax(output)
-
-    print("Input Digit:")
-    print(digits[digit])
-    print("Recognized as:", prediction)
-    print() 
+# Object counting
+object_counts = {}
+for result in results:
+ for box in result.boxes:
+    cls_id = int(box.cls[0])
+ label = model.names[cls_id]
+ object_counts[label] = object_counts.get(label, 0) + 1
+print("Objects Detected:")
+for obj, count in object_counts.items():
+ print(f"• {obj.capitalize():12}: {count}")
